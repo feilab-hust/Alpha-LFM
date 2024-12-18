50a8908357a5fe6cb94a34dcafb32bd848d34adf
CQ-Yi <56756186+CQ-Yi@users.noreply.github.com>
2024-12-18 17:44:04 +0800

    update branches

--- utils/misc/read_psf.m
+++
@@ -1,8 +0,0 @@
-function [LFpsf,height,width,depth,Nnum,CAindex] = read_psf(file)
-    psf = load(file);
-    psf5d = psf.H;
-    Nnum = psf.Nnum;
-    CAindex = psf.CAindex;
-    [height, width, Nx, Ny, depth] = size(psf5d);
-    LFpsf = double(psf5d);
-end
